759c77c3427fac722a9e9213d19af71ea16ea7d2
Dalso13 <flskwhdhk@gmail.com>
2025-03-19 14:27:15 +0900

    feat: 아몬드 쇼핑몰 웹 뷰로 띄우기

--- app/src/main/java/com/jdw/webview_test/MainActivity.kt
+++ app/src/main/java/com/jdw/webview_test/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.jdw.webview_test
 
 import android.os.Bundle
+import android.webkit.WebChromeClient
+import android.webkit.WebView
+import android.webkit.WebViewClient
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -9,12 +12,28 @@ import androidx.core.view.WindowInsetsCompat
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val webView : WebView = findViewById<WebView>(R.id.webView)
+
+        webView.settings.run {
+            javaScriptEnabled = true
+            javaScriptCanOpenWindowsAutomatically = true
+            setSupportMultipleWindows(true)
+        }
+
+        webView.webViewClient = WebViewClient()
+        webView.webChromeClient = WebChromeClient()
+        webView.loadUrl("https://www.almondpetshop.co.kr")
+    }
+
+    override fun onBackPressed() {
+        val webView : WebView = findViewById<WebView>(R.id.webView)
+
+        if (webView.canGoBack()) {
+            webView.goBack()
+        } else {
+            super.onBackPressed()
         }
     }
 }
